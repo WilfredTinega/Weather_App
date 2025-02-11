ec198c7c37638f9472fbd15f15dd9de8c5b5d6fe
wilfredtinega <lowkieman@gmail.com>
2025-02-11 09:13:00 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='min-h-[100vh] bg-slate-500 flex flex-col items-center justify-center text-amber-500'>
+      app
+    </div>
+  )
+}
+
+export default App
